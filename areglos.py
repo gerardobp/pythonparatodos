21d77ebb1f951989801b76fb74b1d7d515e45a6c
yerarbp <yerar_gbp@hotmail.com>
2015-11-16 13:37:54 -0600

    el primer algoritmo

---
+++ areglos.py
@@ -0,0 +1,28 @@
+cads="cadenas \n comillas dobles"
+cads2='esta es una de\n\tcomillas simples'
+l = [1,"hola",True,["hooooola"]]
+l2 = l[0:3]
+
+print cads2
+print l
+print "solo toma los valores de 1 a 3"
+print l2
+
+l3 = l[0::2]
+print l3
+
+
+#cambiar los valores de una lista
+l[0:2] = [3,4]  #cambiar los primeros dos valores por 3y 4
+print l
+
+#sustituir los valores 3 y 4 por solo un valor 
+l[0:2] ="4"
+print l 
+
+l2 = l[-1]
+l3 = l[-2]
+
+print l2
+print l3
+print "A falta de amor , programar en python mejor :3"
